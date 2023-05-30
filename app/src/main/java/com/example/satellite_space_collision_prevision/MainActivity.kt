665dfef715ed48c867b3e0a23f5eaef4df30d97a
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-05-30 17:57:56 +0200

    First commit

---
+++ app/src/main/java/com/example/satellite_space_collision_prevision/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.satellite_space_collision_prevision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
