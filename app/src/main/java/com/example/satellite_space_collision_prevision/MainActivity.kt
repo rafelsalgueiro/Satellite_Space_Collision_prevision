2c82ad9fbc94431245cd434f45a4ed3282cc5466
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-05-30 19:58:19 +0200

    Bug deleted

--- app/src/main/java/com/example/satellite_space_collision_prevision/MainActivity.kt
+++ app/src/main/java/com/example/satellite_space_collision_prevision/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         setContentView(binding.root)
     }
 }
