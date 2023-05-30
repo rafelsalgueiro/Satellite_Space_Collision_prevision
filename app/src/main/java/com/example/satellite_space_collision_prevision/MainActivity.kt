e8a41e2eb2935df93e0dd5355739969a5c1812af
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-05-30 19:00:54 +0200

    Binding added

--- app/src/main/java/com/example/satellite_space_collision_prevision/MainActivity.kt
+++ app/src/main/java/com/example/satellite_space_collision_prevision/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.satellite_space_collision_prevision
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.satellite_space_collision_prevision.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
+
+
