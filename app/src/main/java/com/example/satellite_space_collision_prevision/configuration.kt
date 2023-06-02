58e1e789a4ffe591ce3fab869208c80d37419cf9
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-06-02 20:55:25 +0200

    had to rebuild layouts for an error done

---
+++ app/src/main/java/com/example/satellite_space_collision_prevision/configuration.kt
@@ -0,0 +1,8 @@
+package com.example.satellite_space_collision_prevision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.satellite_space_collision_prevision.databinding.ActivityMainBinding
+
+class configuration {
+}
