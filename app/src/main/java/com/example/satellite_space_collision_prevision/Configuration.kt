b4383345d3788837bd7e147d50b2b3971e99e224
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-06-29 10:19:50 +0200

    SSH connection

--- app/src/main/java/com/example/satellite_space_collision_prevision/Configuration.kt
+++ app/src/main/java/com/example/satellite_space_collision_prevision/Configuration.kt
@@ -1,12 +1,79 @@
-package com.example.satellite_space_collision_prevision
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.satellite_space_collision_prevision.R
 import com.example.satellite_space_collision_prevision.databinding.ConfigurationBinding
-class Configuration : AppCompatActivity(){
+
+class Configuration : AppCompatActivity() {
     private val binding by lazy { ConfigurationBinding.inflate(layoutInflater) }
+    private lateinit var ipAddressEditText: EditText
+    private lateinit var masterPasswordEditText: EditText
+    private lateinit var connectButton: Button
+    private lateinit var statusTextView: TextView
+    private lateinit var sshConnection: SSHConnection
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        ipAddressEditText = binding.IPAddress
+        masterPasswordEditText = binding.MasterPassword
+        connectButton = binding.ConnectButton
+        statusTextView = binding.Status
+
+        connectButton.setOnClickListener {
+            val ipAddress = ipAddressEditText.text.toString()
+            val masterPassword = masterPasswordEditText.text.toString()
+
+            sshConnection = SSHConnection(ipAddress, masterPassword)
+
+            if (sshConnection.connect()) {
+                // Connection established successfully
+                // Perform any operations you need on the SSH connection
+                updateStatusTextView("Connected")
+            } else {
+                // Connection failed
+                updateStatusTextView("Disconnected")
+            }
+        }
+
+        val powerOffButton = binding.powerOffButton
+        val rebootButton = binding.rebootButton
+        val clearKMLDataButton = binding.clearKMLDataButton
+
+        powerOffButton.setOnClickListener {
+            if (sshConnection.isConnected) {
+                val command = "sudo lg-reboot -p" // Command to power off using lg-reboot with sudo
+                sshConnection.executeCommand(command)
+                // Handle the output if needed
+            }
+        }
+
+        rebootButton.setOnClickListener {
+            if (sshConnection.isConnected) {
+                val command = "sudo lg-reboot -r" // Command to reboot using lg-reboot with sudo
+                sshConnection.executeCommand(command)
+                // Handle the output if needed
+            }
+        }
+
+
+        clearKMLDataButton.setOnClickListener {
+            // Perform action to clear KML data
+            // You can call a function here to clear the KML data
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sshConnection.disconnect()
+        updateStatusTextView("Disconnected")
+    }
+
+    private fun updateStatusTextView(status: String) {
+        val statusText = getString(R.string.status_label, status)
+        statusTextView.text = statusText
     }
 }
--- app/src/main/java/utilities/SSHConnection.kt
+++ app/src/main/java/utilities/SSHConnection.kt
@@ -1,27 +1,59 @@
-package utilities;
+import com.jcraft.jsch.ChannelExec
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.Session
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-import net.schmizz.sshj.SSHClient
-import net.schmizz.sshj.transport.verification.PromiscuousVerifier
+class SSHConnection(private val ipAddress: String, private val password: String) {
+    private var session: Session? = null
 
-
-public class SSHConnection {
-
-    fun SSHConnect(host: String, username: String, password: String, command: String): String {
-        val ssh = SSHClient()
-        ssh.addHostKeyVerifier(PromiscuousVerifier())
-
+    val isConnected: Boolean
+        get() = session != null && session!!.isConnected
+    fun connect(): Boolean {
         try {
-            ssh.connect(host)
-            ssh.authPassword(username, password)
+            val jsch = JSch()
+            session = jsch.getSession("lg1", ipAddress, 22)
+            session?.setPassword(password)
+            session?.setConfig("StrictHostKeyChecking", "no")
+            session?.connect()
 
-            val session = ssh.startSession()
-            val commandResult = session.exec(command).inputStream.bufferedReader().use { it.readText() }
-
-
-            return commandResult
+            return true
         } catch (e: Exception) {
             e.printStackTrace()
-            return "Error"
         }
+
+        return false
+    }
+
+    fun disconnect() {
+        session?.disconnect()
+        session = null
+    }
+
+    fun executeCommand(command: String): String {
+        val output = StringBuilder()
+
+        if (session != null && session?.isConnected == true) {
+            try {
+                val channel = session?.openChannel("exec") as ChannelExec
+                channel.setCommand(command)
+
+                val inputStream = channel.inputStream
+                val reader = BufferedReader(InputStreamReader(inputStream))
+
+                channel.connect()
+
+                var line: String?
+                while (reader.readLine().also { line = it } != null) {
+                    output.append(line).append("\n")
+                }
+
+                channel.disconnect()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        return output.toString()
     }
 }
